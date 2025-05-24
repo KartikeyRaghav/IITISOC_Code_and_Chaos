6438d84394d318bbbead6314f8932e786b2556f5
Kartikey Raghav <kartikeyraghav2006@gmail.com>
2025-05-24 09:59:10 +0530

    First Get request created

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config({ path: "./.env" });
 
-app.listen(3000, () => {
-  console.log(3000);
+app.get("/", (req, res) => {
+  console.log("Hello World");
+  res.send("<h1>Hello World</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(process.env.PORT);
 });
