4de0f988a4d3c9e09cc04beeb4b82e4994e6727c
Kartikey Raghav <kartikeyraghav2006@gmail.com>
2025-06-02 16:01:45 +0530

    Added Prettier

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config({ path: "./.env" });
+
+mongoose.connect('mongodb://mongo_ignitia:password@localhost:27017')
 
 app.get("/", (req, res) => {
   console.log("Hello World");
