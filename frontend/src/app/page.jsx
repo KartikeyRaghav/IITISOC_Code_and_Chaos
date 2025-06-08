cbf9d745248aa45ef396ccfb07e90c856e0a3f58
Kartikey Raghav <kartikeyraghav2006@gmail.com>
2025-06-08 20:14:26 +0530

    style: update CSS and JSX formatting for consistency

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,10 +1,9 @@
-import Header from '../../components/Header';
-import Navbar from '../../components/Navbar';
-
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div className='pt-[1rem] overflow-hidden'>
+    <div className="pt-[1rem] overflow-hidden">
       <Header />
       <Navbar />
     </div>
