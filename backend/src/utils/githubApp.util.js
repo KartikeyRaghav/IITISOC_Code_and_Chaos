9f2681ab88c8d82954fdf34e30fce87adfc885c8
kartikeyraghav <sse240021008@iiti.ac.in>
2025-07-28 14:04:56 +0530

    checks

--- backend/src/utils/githubApp.util.js
+++ backend/src/utils/githubApp.util.js
@@ -8,16 +8,25 @@ import fetch from "node-fetch";
 // 1. Verify webhook signature
 export function verifyWebhookSignature(req) {
   const signature = req.headers["x-hub-signature-256"];
+  console.log("signature", signature);
   const payload = req.rawBody || JSON.stringify(req.body);
-  const hmac = crypto.createHmac("sha256", process.env.GITHUB_APP_WEBHOOK_SECRET);
+  console.log("payload", payload);
+  const hmac = crypto.createHmac(
+    "sha256",
+    process.env.GITHUB_APP_WEBHOOK_SECRET
+  );
   const digest = `sha256=${hmac.update(payload).digest("hex")}`;
-
+  console.log("hmac", hmac);
+  console.log("digest", digest);
   return crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(digest));
 }
 
 // 2. Generate GitHub App JWT (10 min lifetime)
 export function generateAppJWT() {
-  const privateKey = readFileSync(process.env.GITHUB_APP_PRIVATE_KEY_PATH, "utf8");
+  const privateKey = readFileSync(
+    process.env.GITHUB_APP_PRIVATE_KEY_PATH,
+    "utf8"
+  );
 
   return jwt.sign(
     {
